2df4e0f42871b28aaebaadd62945a8ff9dee2c9a
Frederico Messa Schwartzhaupt <frederico.messa@gmail.com>
2019-12-03 20:01:55 -0300

    Revert "minor try"
    This reverts commit 1b3e430b0026212e633800977a30aba48b132159.

--- src/support.c
+++ src/support.c
@@ -176,7 +176,8 @@ int format_root_dir(int partition)
     iNode inode;
     define_empty_inode_from_inode_ptr(&inode);
 
-    inode.dataPtr[0] = get_data_block_ptr_given_data_block_number(0);
+    if ((inode.dataPtr[0]  = get_data_block_ptr_given_data_block_number(0)) == INVALID_PTR)
+        return ERROR;
     inode.blocksFileSize += 1;
 
     update_inode_on_disk(0, inode);
